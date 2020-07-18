831f9b8e0a06f7223b4e8955a9fa0d663e7d20df
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-18 15:53:38 +0100

    enhancement: changes

--- api/models.py
+++ api/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 # from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False, unique=True,
-                                related_name = 'user')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False, unique=True, related_name = 'user')
     facebook_user = models.CharField(max_length=50, blank=True, null=True, unique=True )
     phone = models.CharField( blank=True, unique=True, null=True, max_length=19)
     profile = models.ImageField(upload_to='profile/', blank=True, null=True)
-
 
     def __str__(self):
         """one-line docstring for representing the Profile object."""
